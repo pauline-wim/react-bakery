0df31a4bef1b3d1d081a9177ac155daf9c887bfb
pauline-wim <linette.wim@hotmail.com>
2022-02-02 17:29:16 +0100

    bakery (part 1)

--- src/components/List.js
+++ src/components/List.js
@@ -5,17 +5,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class List extends React.Component {
   render() {
     return (
-      <ul className="list-group">
-        <li className="border border-2 list-group-item">
-          <ul>
-            {this.props.items.map((item) => {
-              return (
-                <li>
-                  {item.name} - {item.price} €
+      <ul className="list-group-flush p-3 w-50">
+        <li className="list-group-item p-0">
+          {this.props.items.map((item) => {
+            return (
+              <ul className="list-group">
+                <li className="list-group-item p-2 d-flex justify-content-between align-items-center">
+                  {item.name}
+                  <span className="badge bg-primary rounded-pill">
+                    {item.price}€
+                  </span>
                 </li>
-              );
-            })}
-          </ul>
+              </ul>
+            );
+          })}
         </li>
       </ul>
     );
